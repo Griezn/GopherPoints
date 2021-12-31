53ad2bbfef4f18721fb9f1fb969432d0f6a8be47
Seppe Degryse <80254822+Griezn@users.noreply.github.com>
2021-12-31 01:59:47 +0100

    Added a function to create a list with all the courses. Created a functino to seperate each course into it's own slice.

--- puntenyeet.go
+++ puntenyeet.go
@@ -5,10 +5,16 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/input"
+	"log"
 )
 
 type points struct {
-	vak       string
+	vak  string
+	test toets
+}
+
+type toets struct {
 	onderwerp string
 	points    string
 	pointsmax string
@@ -28,21 +34,116 @@ func handleError(err error) {
 func readPoints(page *rod.Page) []points {
 	//array to return
 	var puntenlijst []points
-	//page.MustElement("#OptionTable > tbody > tr > td.countInput > input").MustSelectAllText().MustInput("").MustInput("50")
+	page.MustElement(`[type="text"]`).MustSelectAllText().MustPress(input.Backspace).MustInput("50")
 
-	for i := 1; i < 20; i++ {
-		//search for the names and add them to the struct
+	log.Println("Start search")
+	for i := 1; i < 50; i++ {
+		//search for the names and add them to the struct points
 		course := page.MustElement(fmt.Sprintf("#\\31 7_2530 > tbody > tr:nth-child(%d) > td.courseNameRecent", i)).MustText()
 		subject := page.MustElement(fmt.Sprintf("#\\31 7_2530 > tbody > tr:nth-child(%d) > td.title", i)).MustText()
 		punten := page.MustElement(fmt.Sprintf("#\\31 7_2530 > tbody > tr:nth-child(%d) > td.point", i)).MustText()
 		puntenmax := page.MustElement(fmt.Sprintf("#\\31 7_2530 > tbody > tr:nth-child(%d) > td.max", i)).MustText()
 
 		puntenlijst = append(puntenlijst, points{
-			vak:       course,
-			onderwerp: subject,
-			points:    punten,
-			pointsmax: puntenmax,
+			vak: course,
+			test: toets{
+				onderwerp: subject,
+				points:    punten,
+				pointsmax: puntenmax,
+			},
 		})
 	}
+	log.Println("End search")
 	return puntenlijst
+}
+
+func removeDuplicateStr(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	var list []string
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
+
+func CreateVakken(points []points) {
+	var vakken []string
+	for i, point := range points {
+		vakken = append(vakken, point.vak)
+		i++
+	}
+	vakken = removeDuplicateStr(vakken)
+	fmt.Println(vakken)
+}
+
+func seperate(points []points) {
+	var Engels []toets
+	var Aardrijkskunde []toets
+	var Fysica []toets
+	var Frans []toets
+	var Duits []toets
+	var Nederlands []toets
+	var Biologie []toets
+	var Chemie []toets
+	var Esthetica []toets
+	var Godsdienst []toets
+	var Wiskunde []toets
+	var restjes []toets
+
+	for i, point := range points {
+		switch point.vak {
+		case "Engels":
+			Engels = append(Engels, point.test)
+			i++
+			continue
+		case "Aardrijkskunde":
+			Aardrijkskunde = append(Aardrijkskunde, point.test)
+			i++
+			continue
+		case "Fysica":
+			Fysica = append(Fysica, point.test)
+			i++
+			continue
+		case "Frans":
+			Frans = append(Frans, point.test)
+			i++
+			continue
+		case "Duits":
+			Duits = append(Duits, point.test)
+			i++
+			continue
+		case "Nederlands":
+			Nederlands = append(Nederlands, point.test)
+			i++
+			continue
+		case "Biologie":
+			Biologie = append(Biologie, point.test)
+			i++
+			continue
+		case "Chemie":
+			Chemie = append(Chemie, point.test)
+			i++
+			continue
+		case "Esthetica":
+			Esthetica = append(Esthetica, point.test)
+			i++
+			continue
+		case "Godsdienst":
+			Godsdienst = append(Godsdienst, point.test)
+			i++
+			continue
+		case "Wiskunde":
+			Wiskunde = append(Wiskunde, point.test)
+			i++
+			continue
+		default:
+			restjes = append(restjes, point.test)
+			i++
+			continue
+		}
+	}
+	fmt.Println(Wiskunde)
 }
