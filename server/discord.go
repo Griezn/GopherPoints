dbdb29382537924cd163e82b1537df28353e8f85
Griezn <seppe.Degryse@telenet.be>
2022-02-07 23:18:25 +0100

    Added discord bot

---
+++ server/discord.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+)
+
+const auth_key = "Njg1ODQyMDE3MTcwNzUxNTEw.XmOiJw.a-DghWGQ33ZcAmW_BJebATQW0To"
+
+var (
+	BotID string
+)
+
+func main() {
+	discord, err := discordgo.New("Bot " + auth_key)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	u, err := discord.User("@me")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	BotID = u.ID
+
+	discord.AddHandler(messageHandler)
+	err = discord.Open()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Bot is running...")
+
+	<-make(chan struct{})
+	return
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	fmt.Println(m.Content)
+	if m.Content == "allo" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "yeet")
+		if err != nil {
+			return
+		}
+	}
+}
