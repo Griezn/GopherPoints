32ba4964050a6ca847f16f51ccd9ebfaef637a7e
Griezn <seppe.Degryse@telenet.be>
2022-02-21 22:54:26 +0100

    Added support for the sql database

--- puntenyeet.go
+++ puntenyeet.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
+	"time"
 
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/input"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type points struct {
@@ -22,20 +26,28 @@ type toets struct {
 	Procent   string `json:"procent"`
 }
 
+type user struct {
+	user_id  int
+	email    string
+	password string
+}
+
 //this logs in to the site and goes tot the recent points
-func login() *rod.Page {
+func (u *user) login() *rod.Page {
 	browser := rod.New().MustConnect()
 	page := browser.MustPage("https://online.myro.be/login.php")
 	page.MustElement("#School").MustInput("tsaam").MustPress(input.Enter)
-	_ = page.MustElement("#Username").Input("seppe.degryse@telenet.be")
-	_ = page.MustElement("td:nth-child(2) > input[type=password]").Input("Sepieboy.268")
+	_ = page.MustElement("#Username").Input(u.email)
+	_ = page.MustElement("td:nth-child(2) > input[type=password]").Input(u.password)
 	page.MustElement("div:nth-child(3) > input[type=submit]").MustClick()
 	page.MustElement("body > table > tbody > tr > td:nth-child(1) > table:nth-child(4) > tbody > tr > td > span:nth-child(2) > a").MustClick()
-	log.Println("Logged in")
+	log.Printf("Logged in as %v", u.email)
 	return page
 }
 
-func readPoints(page *rod.Page) []points {
+//read all the latest 50 points handy for first time login
+func (u *user) readPoints() []points {
+	page := u.login()
 	//array to return
 	var puntenlijst []points
 	page.MustElement(`[type="text"]`).MustSelectAllText().MustPress(input.Backspace).MustInput("50")
@@ -49,10 +61,12 @@ func readPoints(page *rod.Page) []points {
 		puntenmax := page.MustElement(fmt.Sprintf("#\\31 7_2530 > tbody > tr:nth-child(%d) > td.max", i)).MustText()
 		procent := page.MustElement(fmt.Sprintf("#\\31 7_2530 > tbody > tr:nth-child(%d) > td.point", i)).MustAttribute("title")
 
+		new_subject := strings.ReplaceAll(subject, "'", "''")
+
 		puntenlijst = append(puntenlijst, points{
 			Vak: course,
 			Test: toets{
-				Onderwerp: subject,
+				Onderwerp: new_subject,
 				Points:    punten,
 				Pointsmax: puntenmax,
 				Procent:   *procent,
@@ -61,14 +75,42 @@ func readPoints(page *rod.Page) []points {
 	}
 	log.Println("End search")
 
-	//Save the points as a JSON file
-
 	return puntenlijst
 }
 
-func SaveJSON() {
-	page := login()
-	lijst := readPoints(page)
+//get only the latest result
+func (u *user) readLatest() points {
+	page := u.login()
+	//array to return
+	var latestPoint points
+	page.MustElement(`[type="text"]`).MustSelectAllText().MustPress(input.Backspace).MustInput("50")
+
+	log.Println("Start search")
+	//search for the names and add them to the struct points
+	course := page.MustElement("#\\31 7_2530 > tbody > tr:nth-child(1) > td.courseNameRecent").MustText()
+	subject := page.MustElement("#\\31 7_2530 > tbody > tr:nth-child(1) > td.title").MustText()
+	punten := page.MustElement("#\\31 7_2530 > tbody > tr:nth-child(1) > td.point").MustText()
+	puntenmax := page.MustElement("#\\31 7_2530 > tbody > tr:nth-child(1) > td.max").MustText()
+	procent := page.MustElement("#\\31 7_2530 > tbody > tr:nth-child(1) > td.point").MustAttribute("title")
+
+	latestPoint = points{
+		Vak: course,
+		Test: toets{
+			Onderwerp: subject,
+			Points:    punten,
+			Pointsmax: puntenmax,
+			Procent:   *procent,
+		},
+	}
+	log.Println("End search")
+	log.Println(latestPoint)
+
+	return latestPoint
+}
+
+//save the list of points as JSON if we want to
+func (u *user) SaveJSON() {
+	lijst := u.readPoints()
 
 	//write json
 	data, err := json.MarshalIndent(&lijst, "", " ")
@@ -83,6 +125,61 @@ func SaveJSON() {
 	}
 }
 
+//Read the points from myro and add them to the database
+func (u *user) updateDatabse(db *sql.DB) {
+	list := u.readPoints()
+
+	log.Println("Start insert")
+	for _, v := range list {
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO points (owner_id, vak, onderwerp, points, pointsmax, procent) VALUES (%d, '%v', '%v', '%v', '%v', '%v');",
+			u.user_id, v.Vak, v.Test.Onderwerp, v.Test.Points, v.Test.Pointsmax, v.Test.Procent))
+		if err != nil {
+			log.Fatalf("Could not insert! Error; %v", err)
+		}
+		defer insert.Close()
+	}
+	log.Println("End insert")
+}
+
+func (u *user) firstlogin() bool {
+	return true
+}
+
 func main() {
-	SaveJSON()
+	//connect to database
+	db, err := sql.Open("mysql", "test:password@tcp(localhost:3306)/gopherpoints")
+	if err != nil {
+		log.Println("Could not open SQL")
+	}
+	defer db.Close()
+
+	//get the users and add them to a list
+	var userList []user
+	users, err := db.Query("SELECT id, email, ppword FROM users;")
+	if err != nil {
+		log.Fatalf("Could not get the users")
+	}
+	for users.Next() {
+		var id int
+		var email, password string
+		users.Scan(&id, &email, &password)
+
+		userList = append(userList, user{
+			user_id:  id,
+			email:    email,
+			password: password,
+		})
+	}
+
+	//every 20 seconds (for now) loop over the userList and update the database
+	for range time.Tick(time.Second * 20) {
+		log.Println("yeet")
+		for _, u := range userList {
+			if u.firstlogin() {
+				u.updateDatabse(db)
+			} else {
+				u.readLatest()
+			}
+		}
+	}
 }
